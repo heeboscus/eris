6f59e47c9016437fcfff05f45b5e51fed8f6a528
Donovan_DMC <dwddaniels262@gmail.com>
2020-10-28 14:20:53 -0500

    Yikes

--- index.d.ts
+++ index.d.ts
@@ -1,74 +1,139 @@
 import Eris from "eris";
 
 declare namespace Hibiscus {
-    export const VERSION: string
+    // Avoid using the "Function" type.
+    type F = (...args: any[]) => any;
 
-    interface Command {
-        name: string,
-        exec: Function,
-        description?: string,
-        args?: {
-           name: string, 
-           type: "str"|"num"|"member"|"user", 
-           required?: boolean, 
-           useRest?: boolean
-        }[],
-        aliases?: string[],
-        hidden?: boolean,
-        checks: Function[],
-        guildOnly?: boolean,
-        cooldown?: number,
-        path?: string,
-        category?: string,
-        setExec(exec: Function): this,
-        setAliases(aliases: string): this,
-        addCheck(check: Function): this,
-        setCooldown(time: number): this,
-        setArgs(args: {name: string, type: "str"|"num"|"member"|"user", required?: boolean, useRest?: boolean}[]): this
+    interface ArgType {
+        name: string;
+        type: "str" | "num" | "member" | "user";
+        required?: boolean;
+        useRest?: boolean;
     }
 
-    interface Category {
-        name: string,
-        commands: Command[],
-        globalChecks?: Function[],
-        path?: string,
-        addCommand(command: Command): this,
-        setChecks(checks: Function[]): this
+    export class Bot extends Eris.Client {
+        options: Eris.ClientOptions;
+        commandOptions: {
+            prefix: string | string[] | ((m: Eris.Message) => string | string[] | Function);
+            usePrefixSpaces?: boolean;
+            ownerID?: string | string[];
+        };
+        commands: Map<string, Command>;
+        categories: Map<string, Category>;
+        cooldowns: Map<string, Map<string, number>>;
+        async processCommands(msg: Eris.Message);
+        getHelp(ctx: CommandContext, command: Command): string;
+        loadCategory(category: Category);
+        unloadCategory(name: string);
+        reloadCategory(name: string);
+        getCatgeory(name: string): Category;
+        addCommand(cmd: Command);
+        removeCommand(cmd: Command);
+        reloadCommand(cmd: Command);
+        loadEvent(name: string, path: string);
+        unloadEvent(name: string);
+        defaultHelp: Command;
+    }
+
+    export class Category {
+        name: string;
+        commands: Command[];
+        globalChecks?: F[];
+        path?: string;
+        addCommand(command: Command): this;
+        setChecks(checks: F[]): this
+        constructor(opts: {
+            name: string;
+            commands?: Command[];
+            globalChecks?: F[];
+            path?: string;
+        });
     }
 
     interface CommandContext {
-        message: Eris.Message
-        bot: Bot
-        command: Command
-        args: any,
-        prefix: string,
-        send: Eris.TextChannel.send,
-        author: Eris.Member|Eris.User
-        channel: Eris.TextChannel|Eris.PrivateChannel,
-        guild: Eris.Guild
+        message: Eris.Message;
+        bot: Bot;
+        command: Command;
+        args: any;
+        prefix: string;
+        send: Eris.TextChannel.send;
+        author: Eris.Member | Eris.User;
+        channel: Eris.TextChannel | Eris.PrivateChannel;
+        guild: Eris.Guild;
     }
 
-    interface Bot extends Eris.Client {
-        options: Eris.ClientOptions,
-        commandOptions: {
-            prefix: string|string[]|((m: Eris.Message) => string|string[]|Function), 
-            usePrefixSpaces?: boolean, 
-            ownerID?: string|string[]
-        },
-        commands: Map<string, Command>,
-        categories: Map<string, Category>,
-        cooldowns: Map<string, Map<string, number>>,
-        async processCommands(msg: Eris.Message),
-        getHelp(ctx: CommandContext, command: Command): string,
-        loadCategory(category: Category),
-        unloadCategory(name: string),
-        reloadCategory(name: string),
-        getCatgeory(name: string): Category,
-        addCommand(cmd: Command), 
-        removeCommand(cmd: Command),
-        reloadCommand(cmd: Command),
-        loadEvent(name: string, path: string),
-        unloadEvent(name: string),
-        defaultHelp: Command
+    interface IsOwnerCheck {
+        (ctx: CommandContext): boolean;
     }
+
+    export type Checks = {
+        isOwner: IsOwnerCheck;
+    };
+
+    export class Command {
+        name: string;
+        exec: F;
+        description?: string;
+        args?: {
+            name: string;
+            type: "str" | "num" | "member" | "user";
+            required?: boolean;
+            useRest?: boolean;
+        }[];
+        aliases?: string[];
+        hidden?: boolean;
+        checks: F[];
+        guildOnly?: boolean;
+        cooldown?: number;
+        path?: string;
+        category?: string;
+        setExec(exec: Function): this;
+        setAliases(aliases: string): this;
+        addCheck(check: Function): this;
+        setCooldown(time: number): this;
+        setArgs(args: ArgType[]): this;
+        constructor(opts: {
+            name: string;
+            exec?: F;
+            description?: string;
+            args: ArgType[];
+            aliases?: string[];
+            hidden?: boolean;
+            checks?: F[];
+            cooldown?: number;
+            guildOnly?: boolean;
+            path?: string;
+            category?: string;
+        });
+    }
+
+    export interface Utils {
+        duration(date1: number, date2: number): string;
+    }
+
+    export class Embed {
+        set<K extends keyof Eris.EmbedOptions>(key: K, value: Eris.EmbedOptions[K]): this;
+        setTitle(title: string): this;
+        setDescription(description: string): this;
+        setURL(url: string): this;
+        setColor(color: number): this;
+        setTimestamp(timestamp: number | string): this;
+        setFooter(text: string, icon_url?: string): this;
+        setThumbnail(url: string);
+        setImage(url: string);
+        setAuthor(name: string, url?: string, icon_url?: string);
+        addField(...args: Eris.EmbedField[]): this;
+        readonly data: {
+            embed: Eris.EmbedOptions;
+        };
+        constructor(opts?: Eris.EmbedOptions);
+    }
+
+    export function whenMentioned(): string[];
+    export function whenMentionedOr(basePrefix: any): typeof whenMentioned;
+
+    export const VERSION: string
+
 }
+
+export = Hibiscus;
--- src/embed.js
+++ src/embed.js
@@ -1,64 +1,64 @@
 class Embed {
-    constructor(opts=undefined) {
-        if (!opts) opts = {}
-        const { title, description, author, url, color, timestamp, fields } = opts
+    constructor(opts = undefined) {
+        if (!opts) opts = {};
+        const { title, description, author, url, color, timestamp, fields } = opts;
         this.data = {
             embed: {
-                title: title,
-                description: description,
-                author: author,
-                url: url,
-                color: color,
-                timestamp: timestamp,
-                fields: fields
+                title,
+                description,
+                author,
+                url,
+                color,
+                timestamp,
+                fields
             }
-        }
-        this.toJSON = () => this.data
+        };
+        this.toJSON = () => this.data;
     }
     set(key, value) {
-        this.data.embed[key] = value
-        return this
+        this.data.embed[key] = value;
+        return this;
     }
     setTitle(title) {
-        this.data.embed.title = title
-        return this
+        this.data.embed.title = title;
+        return this;
     }
     setDescription(description) {
-        this.data.embed.description = description
-        return this
+        this.data.embed.description = description;
+        return this;
     }
     setURL(url) {
-        this.data.embed.url = url
-        return this
+        this.data.embed.url = url;
+        return this;
     }
     setColor(color) {
-        this.data.embed.color = color
-        return this
+        this.data.embed.color = color;
+        return this;
     }
     setTimestamp(timestamp) {
-        this.data.embed.timestamp = timestamp
-        return this
+        this.data.embed.timestamp = timestamp;
+        return this;
     }
-    setFooter(text, icon_url=undefined) {
-        this.data.embed.footer = {text: text, icon_url: icon_url}
-        return this
+    setFooter(text, icon_url = undefined) {
+        this.data.embed.footer = { text: text, icon_url: icon_url };
+        return this;
     }
     setThumbnail(url) {
-        this.data.embed.thumbnail = {url: url}
-        return this
+        this.data.embed.thumbnail = { url: url };
+        return this;
     }
     setImage(url) {
-        this.data.embed.image = {url: url}
-        return this
+        this.data.embed.image = { url: url };
+        return this;
     }
-    setAuthor(name, url=undefined, icon_url=undefined) {
-        this.data.embed.author = {name: name, url: url, icon_url: icon_url}
-        return this
+    setAuthor(name, url = undefined, icon_url = undefined) {
+        this.data.embed.author = { name: name, url: url, icon_url: icon_url };
+        return this;
     }
-    addField(name, value, inline=false) {
-        this.data.embed.fields.push({name: name, value: value, inline: inline ? inline : false})
-        return this
+    addField(name, value, inline = false) {
+        this.data.embed.fields.push({ name: name, value: value, inline: inline ? inline : false });
+        return this;
     }
 }
 
-module.exports = Embed
+module.exports = Embed;
